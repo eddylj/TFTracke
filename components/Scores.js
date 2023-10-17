92cd623644db7d989644611b5bccfe45603c2aaa
Eddy <eddyljzhang@hotmail.com>
2023-10-17 11:21:23 +1100

    changed style

---
+++ components/Scores.js
@@ -0,0 +1,35 @@
+"use client";
+
+const Scores = (props) => {
+  const Num = ({ number }) => {
+    let className = "inline-flex items-center justify-center w-4 rounded-md px-3 py-2 text-xs font-medium ring-1 ring-inset ring-gray-500/10 ";
+
+    if (number === 1) {
+      className += "text-black bg-[#FFD700]";
+    } else if (number === 2) {
+      className += "text-black bg-gray-100";
+    } else if (number === 3) {
+      className += "text-black bg-[#CD7F32]";
+    } else if (number === 4) {
+      className += "text-black bg-gray-300";
+    } else if (number >= 5 && number <= 8) {
+      className += "text-white bg-gray-600";
+    }
+
+    return (
+      <><div className={className}>
+        {number}
+      </div>&nbsp;</>
+
+    );
+  }
+
+  return (
+    <div className='flex py-3 pl-2 flex-wrap gap-x-0 gap-y-2'>
+      {props.last20.map((number, index) => (
+        <Num key={index} number={number}></Num>
+      ))}
+    </div>);
+}
+
+export default Scores
---
+++ components/TFTStats.js
@@ -0,0 +1,73 @@
+"use client";
+
+import React, { useState } from "react";
+import { Orbit } from "@uiball/loaders";
+import Scores from "./Scores";
+
+const TFTStats = () => {
+  const [clicked, setClicked] = useState(false);
+  const [emptyGameName, setEmptyGameName] = useState(false);
+  const [gameName, setGameName] = useState("");
+  const [averagePlacement, setAveragePlacement] = useState(null);
+  const [last20, setLast20] = useState(null);
+
+  const handleGameNameChange = (event) => {
+    setGameName(event.target.value);
+  };
+
+  const fetchTFTStats = () => {
+    setAveragePlacement(null);
+    if (gameName.trim() === "") {
+      setEmptyGameName(true);
+      return;
+    }
+    else { setEmptyGameName(false); }
+    setClicked(true);
+    fetch(`/api/tftStats?gameName=${gameName}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setAveragePlacement(data.avgPlacement);
+        setLast20(data.last_20);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  return (
+    <div className="w-1/2 flex justify-center flex-col">
+      <div className="pb-5">
+        <h1 className="font-bold text-xl py-5">TFT Stats</h1>
+        <input
+          className="shadow appearance-none border rounded w-3/4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          value={gameName}
+          onChange={handleGameNameChange}
+          placeholder="Enter in game name"
+        />
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={fetchTFTStats}>Fetch Stats</button>
+      </div>
+
+      <div>
+        {
+          clicked ? (
+            averagePlacement !== null && !isNaN(averagePlacement) ? (
+              <>
+                <p>Average Placement in Last {last20.length} Games: {averagePlacement.toFixed(2)}</p>
+                <Scores last20={last20} />
+              </>
+            ) : (
+              <div>
+                <Orbit size={35} color="#231F20" />
+              </div>
+            )
+          ) : null
+        }
+
+        {emptyGameName && <p className="text-red-600">Game Name cannot be empty</p>}
+      </div>
+    </div >
+  );
+};
+
+export default TFTStats;
